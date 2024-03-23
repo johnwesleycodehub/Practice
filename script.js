155a5a6cbec315e2bedc1dd4dc73dec9ce8f63fa
johnwesleycodehub <johnwesley2307r@gmail.com>
2024-03-23 11:22:37 +0530

    complete

---
+++ script.js
@@ -0,0 +1,10 @@
+var movie ={
+    Title:"Leo",
+    Hero:"Vijay",
+    Heroine:"Trisa",
+    Director:"Lokesh Kanagaraj",
+    Imdbrating:7.5,
+    Budget:500,
+    Cast:["GVM","Arjun","Myskin","Sandy","Janani","Madanona","Sanjay dutt"]
+}
+movie.Languages=["Tamil,Hindi"]
