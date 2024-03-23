8c3e4239780454031c44411731746f2d0263ce50
johnwesleycodehub <johnwesley2307r@gmail.com>
2024-03-23 11:58:16 +0530

    updated

--- script.js
+++ script.js
@@ -1,10 +1,57 @@
-var movie ={
-    Title:"Leo",
-    Hero:"Vijay",
-    Heroine:"Trisa",
-    Director:"Lokesh Kanagaraj",
-    Imdbrating:7.5,
-    Budget:500,
-    Cast:["GVM","Arjun","Myskin","Sandy","Janani","Madanona","Sanjay dutt"]
+// var movie ={
+//     Title:"Leo",
+//     Hero:"Vijay",
+//     Heroine:"Trisa",
+//     Director:"Lokesh Kanagaraj",
+//     Imdbrating:7.5,
+//     Budget:500,
+//     Cast:["GVM","Arjun","Myskin","Sandy","Janani","Madanona","Sanjay dutt"]
+// }
+// movie.Languages=["Tamil,Hindi"]
+// console.log(movie);
+
+// var person = {
+//     name: "rupan",
+//     age:15,
+//     Gender:"male"
+// }
+// var b =JSON.stringify(person);
+// console.log(b);
+// var c = JSON.parse(b)
+// console.log(c);
+
+var arr =[{
+name: "John Wesley",
+Age: 22
+},
+{
+    name: "Wesley",
+    Age: 23
+}]
+for(var i=0;i<arr.length;i=i+1)
+{
+    console.log(arr[i].name,arr[i].Age);
 }
-movie.Languages=["Tamil,Hindi"]
+
+var arr = [
+    {
+        "name":"rupan",
+        "age":"15"
+    },
+    {
+        "name":"chakravarthy",
+        "age":"05"
+    },
+    {
+        "name":"John",
+        "age":"30"
+    },
+    {
+        "name":"Doe",
+        "age":"40"
+    }
+]
+for(var i=0;i<arr.length;i=i+1){
+    console.log(arr[i].name,arr[i].age);
+    
+}
